759048092ebcfa2fe1eb5b8275ca55d6170a7b39
bdbrink <brendan.brink@outlook.com>
2023-04-13 14:35:10 -0500

    gathering urls for api calls

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,3 +1,10 @@
+import datetime
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    API_KEY = "bd8a0a0778426c96165226c224399ce2"
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid"
+
