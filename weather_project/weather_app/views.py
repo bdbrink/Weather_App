910ecd61426541fcf98dfce50eebddc734199fcd
bdbrink <brendan.brink@outlook.com>
2023-04-17 17:41:13 -0500

    adding post request and html template

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -6,5 +6,10 @@ from django.shortcuts import render
 def index(request):
     API_KEY = "bd8a0a0778426c96165226c224399ce2"
     current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
-    forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid"
+    forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
+
+    if request.method == "POST":
+        pass
+    else:
+        return render(request, "weather_app/index.html")
 
