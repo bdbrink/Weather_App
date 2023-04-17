864e4ef698068ae654a790573df00b1eec7098c6
bdbrink <brendan.brink@outlook.com>
2023-04-17 17:45:13 -0500

    add one city requirement, second city can return none if no comparsion is asked

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -9,7 +9,7 @@ def index(request):
     forecast_url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}"
 
     if request.method == "POST":
-        pass
+        city1 = request.POST['city1']
+        city2 = request.get('city2', None)
     else:
         return render(request, "weather_app/index.html")
-
